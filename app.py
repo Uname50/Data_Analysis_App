d4a7e36eb3ea626ad8bc19e4095224dfa5a35714
Dmitrii I <estaquito@outlook.com>
2024-12-04 16:10:10 -0500

    added GCS functionality

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pathlib import Path
-from data_processing import process_uploaded_file
+from data_processing import process_uploaded_file, save_and_upload_processed_data
 
 app = FastAPI()
 
 # Store the files locally for now
 UPLOAD_DIR = Path("uploads")
 UPLOAD_DIR.mkdir(exist_ok=True)
+
+# GCP bucket name
+BUCKET_NAME = "data-analysis-app-bucket "
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
@@ -39,14 +42,21 @@ async def list_files():
     return{"files": files}
 
 @app.post("/process/")
-async def process_file(file_name: str):
+async def process_file_and_upload(file_name: str):
     
     """
-    Endpoint to process an uploaded file and extract insights
+    Endpoint to process a file and upload the results to GCS
     """
 
     file_path = UPLOAD_DIR / file_name 
     if not file_path.exists():
         raise HTTPException(status_code=404, detail="File not found")
     
-    return process_uploaded_file(file_path)
+    # Process file and get a DF
+    df = process_uploaded_file(file_path)
+    
+    # Save processed data locally and upload to GCS
+    output_file_path = Path("processed_data.csv")
+    gcs_url = save_and_upload_processed_data(df, output_file_path, BUCKET_NAME)
+
+    return {"message": "File processed and uploaded successfully", "gcs_url": gcs_url}
--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+from google.cloud import storage
 
 def process_uploaded_file(file_path: Path) -> dict:
     
@@ -24,3 +25,34 @@ def process_uploaded_file(file_path: Path) -> dict:
         return {"sales_by_category": sales_by_category.to_dict(orient="records")}
     else:
         return{"message": "Required columns are not found in the DF"}
+
+def upload_to_gcs(bucket_name: str, source_file_path: Path, destination_blob_name: str):
+    
+    """
+    Uploads file to Google Cloud Storage
+    """
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Upload the file
+    blob.upload_from_filename(str(source_file_path))
+
+    return blob.public_url
+
+def save_and_upload_processed_data(df: pd.DataFrame, output_file_path: Path, bucket_name: str):
+
+    """
+    Save the processed DF into a CSV file and upload it to GCS
+    """
+
+    # Save the DF to CSV
+    df.to_csv(output_file_path, index=False)
+
+    # Upload the CSV
+    file_name = output_file_path.name
+    gcs_url = upload_to_gcs(bucket_name, output_file_path, file_name)
+
+    return gcs_url
+
